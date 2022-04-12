2c61bc49c46ec937bae67bcb56d692716722ad95
Alexander Kononykhin <alexander.kononykhin@corp.mail.ru>
2022-04-12 13:45:13 +0300

    fix middleware

--- middleware/interceptor.go
+++ middleware/interceptor.go
@@ -9,22 +9,19 @@ import (
 	"google.golang.org/grpc"
 )
 
-// DBClientProvider is a func for wrapping database
-type DBClientProvider func(ctx context.Context) db.IClient
-
 // NewDBServerInterceptor wrap endpoint with middleware mixing in db connection
-func NewDBServerInterceptor(dbClientProvider DBClientProvider, option ...db.Option) grpc.UnaryServerInterceptor {
+func NewDBServerInterceptor(dbClient db.IClient, option ...db.Option) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
-		return handler(db.NewContext(ctx, dbClientProvider(ctx).WrapWithContext(ctx), option...), req)
+		return handler(db.NewContext(ctx, dbClient.WrapWithContext(ctx), option...), req)
 	}
 }
 
 // NewDBServerMiddleware wrap endpoint with middleware mixing in db connection
-func NewDBServerMiddleware(dbClientProvider DBClientProvider, option ...db.Option) func(next http.Handler) http.Handler {
+func NewDBServerMiddleware(dbClient db.IClient, option ...db.Option) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ctx := r.Context()
-			r = r.WithContext(db.NewContext(ctx, dbClientProvider(ctx).WrapWithContext(ctx), option...))
+			r = r.WithContext(db.NewContext(ctx, dbClient.WrapWithContext(ctx), option...))
 			next.ServeHTTP(w, r)
 		})
 	}
